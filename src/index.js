d78ee7b8a58e96a7fb2ccd51fb42cf96cde5cd58
Natosha <lalabumlala@gmail.com>
2022-01-11 19:14:46 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    // if (n < 0) {
+    //     return +String(-n).split('').reverse().join('')
+    // }
+    //     return +String(n).split('').reverse().join('')
+    return (n>=0) ? +String(n).split('').reverse().join('') : +String(-n).split('').reverse().join('')
 }
